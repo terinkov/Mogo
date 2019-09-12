db43905c82b901e4edfe3871184a2186e4a7ee9b
terinkov <aulyadurov@mail.ru>
2019-09-12 23:25:50 +0300

    vo vo ter' vso poluchilos'(no ne zacrivanie pri vklyuchenii drygogo item'a)

--- js/script.js
+++ js/script.js
@@ -8,10 +8,18 @@ dropdowns.forEach(function(item, i){
             if(number!=i){
                 cur.children[2].classList.remove("do__dropdown-arrow_rotated_up"); 
             }
-            cur.children[3].style.display = "none";   
+            document.querySelectorAll(".do__dropdown-content")[i].style.display = "none";
+            dropdowns[i].style.marginBottom = "10px";
+            dropdowns[i].style.borderBottom = "1px solid #e5e5e5";
+            // cur.children[3].style.display = "none";   
         });
         dropdowns[i].children[2].classList.toggle("do__dropdown-arrow_rotated_up");
-        if(dropdowns[i].children[2].classList.contains('do__dropdown-arrow_rotated_up')) dropdowns[i].children[3].style.display = "block";
+        if(dropdowns[i].children[2].classList.contains('do__dropdown-arrow_rotated_up')) {
+            document.querySelectorAll(".do__dropdown-content")[i].style.display = "block";
+            dropdowns[i].style.marginBottom = "0px";
+            dropdowns[i].style.borderBottom = "none";
+            
+        }
 
     });
 });
