afe9bac68784509c2ce03e8b7d6a0f7255f9c007
terinkov <aulyadurov@mail.ru>
2019-09-14 22:22:09 +0300

    FKLUSDF:LSDKFJ>SLDKFJS:LDKFJ:SDLKFj

--- js/script.js
+++ js/script.js
@@ -1,32 +1,121 @@
-let indexRotate = 0;
 let dropdowns = document.querySelectorAll(".do__dropdown");
-dropdowns.forEach(function(item, i){
-    item.addEventListener("click", function(e){
-        console.log(i);
-        console.log(dropdowns[i].children[2]);
-        dropdowns.forEach(function(cur, number){
-            if(number!=i){
-                cur.children[2].classList.remove("do__dropdown-arrow_rotated_up"); 
-            }
-            document.querySelectorAll(".do__dropdown-content")[i].style.display = "none";
-            dropdowns[i].style.marginBottom = "10px";
-            dropdowns[i].style.borderBottom = "1px solid #e5e5e5";
-            // cur.children[3].style.display = "none";   
-        });
-        dropdowns[i].children[2].classList.toggle("do__dropdown-arrow_rotated_up");
-        if(dropdowns[i].children[2].classList.contains('do__dropdown-arrow_rotated_up')) {
-            document.querySelectorAll(".do__dropdown-content")[i].style.display = "block";
-            dropdowns[i].style.marginBottom = "0px";
-            dropdowns[i].style.borderBottom = "none";
-            
-        }
+let contentes = document.querySelectorAll(".do__dropdown-content");
+let icons = [false, false, false];
+let texts = [false, false, false];
+let current;
 
+dropdowns.forEach(function(item, i){
+    item.addEventListener("click", function(e) {
+        
+        let anyOpened = false;
+
+
+        current = i;
+        if(icons[i]==true)
+            icons[i] = false;
+        else
+            icons[i] = true;
+        console.log(i + " " + icons[i]);
+        console.log(icons);//+
+        nullify(icons);
+        live(icons, i);
+        // showContent(icons);
+        
+
+        
+        
+        
+        // parity++;
+        // opened = parity % 2 == 0 ? false : true;
+        // console.log(opened);
+        // console.log("i " + i);
+        // console.log("current " + current);
+        // dropdowns.forEach(function(cur, number){
+        //     if(number!=i){
+        //         cur.children[2].classList.remove("do__dropdown-arrow_rotated_up"); 
+        //     }
+        //     document.querySelectorAll(".do__dropdown-content")[i].style.display = "none";
+        //     dropdowns[i].style.marginBottom = "10px";
+        //     dropdowns[i].style.borderBottom = "1px solid #e5e5e5";
+        //     // cur.children[3].style.display = "none";  
+        // });
+        // document.querySelectorAll(".do__dropdown-content").forEach(function(content, contentNumber){
+        //     content.style.display = "none";
+        //     dropdowns[i].style.marginBottom = "10px";
+        //     dropdowns[i].style.borderBottom = "1px solid #e5e5e5";
+        // });
+        // dropdowns[i].children[2].classList.toggle("do__dropdown-arrow_rotated_up");
+        // if(dropdowns[i].children[2].classList.contains('do__dropdown-arrow_rotated_up')) {
+        //     document.querySelectorAll(".do__dropdown-content")[i].style.display = "block";
+        //     dropdowns[i].style.marginBottom = "0px";
+        //     dropdowns[i].style.borderBottom = "none";
+        // }
+        // if(current!=i) {
+        //     anAnotherActivity=true;
+        //     console.log('activity');
+        // }
+        // if(opened && anAnotherActivity){
+        //     dropdowns[current].style.marginBottom = "10px";
+        //     dropdowns[current].style.borderBottom = "1px solid #e5e5e5";
+        //     console.log(current + " 10px");
+        // }
+        // current = i;
+        anyOpened = check(icons);
     });
 });
+
+function showContent(array){
+    texts = [false, false, false];
+    console.log(findOpened(array));
+    console.log(array);
+    texts[findOpened(array)] = true;
+    contentes[findOpened(texts)].style.display = "block";
+    console.log(array);
+    console.log("------------");
+}
+
+function live(icons, index){
+    let opened = findOpened(icons);
+    icons[index] = dropdowns[index].children[2].classList.toggle("do__dropdown-arrow_rotated_up");
+    console.log(icons);
+    console.log(index + " " + icons);
+}
+
+function findOpened(array){
+    let index;
+    array.forEach(function(item, i){
+        if(item == true){
+            index = i;
+        }
+    });
+    return index;
+}
+
+function check(a){
+    let allFalse = true;
+    a.forEach(function(item){if(item==true) allFalse=false;});
+    return !allFalse;
+    //if any opened - true
+}
 
 function childWithClass(item, ofClass, or){
     item.children.forEach(function(child, i){
         if(child.contains(ofClass) || child.contains(or)) return child;
     });
     return null;
+}
+
+function nullify(array){
+    array.forEach(function(item, i){
+        if(!item){
+            console.log(dropdowns[i]);
+            console.log(dropdowns[i].children[2]);
+            dropdowns[i].children[2].classList.remove("do__dropdown-arrow_rotated_up");
+            console.log(i);
+        }
+    });
+}
+
+function restore(item){
+    
 }
