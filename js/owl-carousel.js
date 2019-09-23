8495b2bd5dbbcff10390710ad8f6661694f45112
terinkov <aulyadurov@mail.ru>
2019-09-23 22:39:46 +0300

    added owl carousel and all adaptations without smallest so must fix bug in card and in team logos set first dn then when hover add df

---
+++ js/owl-carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        mouseDrag: false,
+        rewind: true,
+        navText: ['<i class="fas fa-chevron-left"></i>',
+        '<i class="fas fa-chevron-right"></i>'],
+        autoplay:true, // автопрокрутка
+        autoplayTimeout:3000, // задержка в мс
+        autoplayHoverPause:false, // не останавливать при наведении
+    });
+});
